537ba72e65534ec956bb914cc111be8a4fd5316f
marceloicampos <marcelo.ildefonso@hotmail.com>
2021-02-01 22:29:02 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Bem Vindo a Imersão React 01 27</p>
+        <p>Bem Vindo a Imersão React - Finalizado em 2021 02 01</p>
         <a
           className="App-link"
           href="https://reactjs.org"
